4aa7e742bbad09cfd85f2a0cb5169d9869455371
Li Xueli <lixueli93@gmail.com>
2019-11-02 12:59:19 +0800

    fix i18n: zh-cn to zh

--- src/components/languageSwitcher.js
+++ src/components/languageSwitcher.js
@@ -25,7 +25,7 @@ class LanguageSwitcher extends Component {
     return (
       <div className="language-switcher">
         {languages.map((language, i) => {
-          if (currentLang.indexOf(language.code) === 0) {
+          if (currentLang && currentLang.indexOf(language.code) === 0) {
             return (
               <React.Fragment key={language.code}>
                 <span className="current">{language.label}</span>
